c6032845ff26ac97ea5598f1228789a00bbaf695
a1769988917 <43460305+a1769988917@users.noreply.github.com>
2019-05-06 16:47:52 +0800

    服务提供端

---
+++ microservice-simple-porvider-user/src/main/java/com/small/cloud/microservicesimpleporvideruser/MicroserviceSimplePorviderUserApplication.java
@@ -0,0 +1,12 @@
+package com.small.cloud.microservicesimpleporvideruser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceSimplePorviderUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceSimplePorviderUserApplication.class, args);
+	}
+}
